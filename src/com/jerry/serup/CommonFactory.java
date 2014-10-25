b19a4680b22ef45d8545fa659788caf88334740a
JerryWang <jerry002@126.com>
2014-10-25 17:40:10 +0800

    普通工厂模式

---
+++ src/com/jerry/serup/CommonFactory.java
@@ -0,0 +1,50 @@
+package com.jerry.serup;
+
+/**
+ * 普通工厂模式
+ * @author Jerry Wang
+ *
+ */
+
+interface Sender {
+	public void send();
+}
+
+class MailSender implements Sender {
+
+	@Override
+	public void send() {
+		System.out.println("this is mail sender");
+	}
+	
+}
+
+class SmsSender implements Sender {
+
+	@Override
+	public void send() {
+		System.out.println("this is sms sender");
+	}
+	
+}
+
+class SendFactory {
+	public Sender produce(String type) {
+		if("mail".equals(type)) {
+			return new MailSender();
+		} else if("sms".equals(type)) {
+			return new SmsSender();
+		} else {
+			return null;
+		}
+	}
+}
+
+public class CommonFactory {
+
+	public static void main(String[] args) {
+		SendFactory factory = new SendFactory();
+		Sender sender = factory.produce("sms");
+		sender.send();
+	}
+}
