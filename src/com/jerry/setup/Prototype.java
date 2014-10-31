ba198a85680756aa6956fbebffbcac46ca310f25
JerryWang <jerry002@126.com>
2014-11-01 02:29:49 +0800

    原型模式 增加深复制

--- src/com/jerry/setup/Prototype.java
+++ src/com/jerry/setup/Prototype.java
@@ -1,5 +1,11 @@
 package com.jerry.setup;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * 原型模式
  * @author Jerry Wang
@@ -17,27 +23,30 @@ public class Prototype implements Cloneable{
 		this.name = name;
 	}
 	
-	public Object clone(){
-        try {
-            return super.clone();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+	/**
+	 * 浅复制：将一个对象复制后，基本数据类型的变量都会重新创建，而引用类型，指向的还是原对象所指向的。
+	 */
+	public Object clone() throws CloneNotSupportedException{
+		Prototype prototype = (Prototype) super.clone();
+        return prototype;
     }
 	
-	public static void main(String[] args) {
-		Prototype pro = new ConcretePrototype("prototype");
-		Prototype pro2 = (Prototype) pro.clone();
-		System.out.println(pro.getName());
-		System.out.println(pro2.getName());
-	}
-}
-
-
-class ConcretePrototype extends Prototype {
-
-	public ConcretePrototype(String name) {
-		super.setName(name);
-	}
-}
+	/**
+	 * 深复制：将一个对象复制后，不论是基本数据类型还有引用类型，都是重新创建的。简单来说，就是深复制进行了完全彻底的复制，而浅复制不彻底。
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+    public Object deepClone() throws IOException, ClassNotFoundException {  
+  
+        /* 写入当前对象的二进制流 */  
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+        ObjectOutputStream oos = new ObjectOutputStream(bos);  
+        oos.writeObject(this);  
+  
+        /* 读出二进制流产生的新对象 */  
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());  
+        ObjectInputStream ois = new ObjectInputStream(bis);  
+        return ois.readObject();  
+    }  
+}	
